061e9e2cc6f4acebfecfac25ffbef3d3510aab56
Vasavi <85066181+vasavivk@users.noreply.github.com>
2024-02-11 11:58:11 +0530

    Add files via upload

---
+++ WebStreamer/__init__.py
@@ -0,0 +1,8 @@
+ 
+
+import time
+from .vars import Var
+from WebStreamer.bot.clients import StreamBot
+
+__version__ = "2.2.4"
+StartTime = time.time()
---
+++ WebStreamer/vars.py
@@ -0,0 +1,30 @@
+ 
+import sys
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Var(object):
+    MULTI_CLIENT = False
+    API_ID = 23028247
+    API_HASH = "659c5f1124a81ad789a6ea021da73f4d"
+    BOT_TOKEN = "6810418594:AAFKZkzxQ9YkP3By6jdzu8m-Dhimiu0im0c"
+    SLEEP_THRESHOLD = 20  # 1 minte
+    WORKERS = 60  # 6 workers = 6 commands at once
+    BIN_CHANNEL = None  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
+    PORT = 8080
+    BIND_ADDRESS = "0.0.0.0"
+    PING_INTERVAL = 1200
+    HAS_SSL = 'true'
+    NO_PORT = "true"
+    HASH_LENGTH = 6
+    FQDN = BIND_ADDRESS
+    URL = "http{}://{}{}/".format(
+            "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
+        )
+    KEEP_ALIVE = 'true'
+    DEBUG = "true"
+    USE_SESSION_FILE = 'true'
+    ALLOWED_USERS = []
